3140d25c1d8c5a3e0c27f6e956b63953f09a9b30
JustWhyy69 <kayla.merchant10@gmail.com>
2022-10-27 12:48:35 -0400

    birthday

---
+++ animal.py
@@ -0,0 +1,12 @@
+class animal(): 
+    def __init__(self):
+        self.name = "name"
+        self.size = 0
+        self.type = 0
+        self.age = 0    
+
+    def speak():
+        pass
+
+    def describe():
+        pass
